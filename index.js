53df9d59e2b9aa9c507ef0622495300bf6ef3303
Satyadeepsb <satyadeepsb@gmail.com>
2023-01-02 16:43:38 +0530

    added error handling

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.log(error);
+    }
     console.log("Connected !!");
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log("accounts", accounts);
     document.getElementById("connectButton").innerHTML = "Connected";
   } else {
     document.getElementById("connectButton").innerHTML =
