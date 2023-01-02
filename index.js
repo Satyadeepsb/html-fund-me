5de94e974d2377022903deec31d268296641fa1e
Satyadeepsb <satyadeepsb@gmail.com>
2023-01-02 17:34:15 +0530

    downloaded and imported ethers.js

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+import { ethers } from "./ethers-5.2.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+console.log(ethers)
+
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
-    try {
-      await window.ethereum.request({ method: "eth_requestAccounts" });
-    } catch (error) {
-      console.log(error);
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await window.ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        console.log("Connected !!")
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        })
+        console.log("accounts", accounts)
+        connectButton.innerHTML = "Connected"
+    } else {
+        connectButton.innerHTML = "Please install metamask"
     }
-    console.log("Connected !!");
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    console.log("accounts", accounts);
-    document.getElementById("connectButton").innerHTML = "Connected";
-  } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install metamask";
-  }
+}
+async function fund(ethAmount) {
+    console.log(`Funding: `)
+    console.log(ethAmount)
+    if (typeof window.ethereum !== "undefined") {
+        // provider / connection to the blockchain
+        // signer / wallet / someone with gas
+        // contract that we are interacting with
+        // ^ ABI & Address
+    }
 }
